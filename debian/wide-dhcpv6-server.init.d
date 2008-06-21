21cc19c49179d517b3a9b3d64411d59e05d021a6
Jeremie Corbier <jeremie.corbier@resel.enst-bretagne.fr>
2008-06-21 16:32:46 +0200

    Make it possible to handle several interfaces with the server...
    ...and get rid of dhcp6ctl in wide-dhcp6-server
    
    Signed-off-by: Jeremie Corbier <jeremie.corbier@resel.enst-bretagne.fr>

--- debian/wide-dhcpv6-server.init.d
+++ debian/wide-dhcpv6-server.init.d
@@ -13,7 +13,7 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 DHCP6SBIN=/usr/sbin/dhcp6s
-DHCP6SPID=/var/run/dhcp6s.pid
+DHCP6SPIDBASE=/var/run/dhcp6s
 NAME="dhcp6s"
 DESC="WIDE DHCPv6 server"
 
@@ -36,37 +36,43 @@ fi
 # single arg is -v for messages, -q for none
 check_status()
 {
-    if [ ! -r "$DHCP6SPID" ]; then
-        test "$1" != -v || echo "$NAME is not running."
-        return 3
-    fi
-    if read pid < "$DHCP6SPID" && ps -p "$pid" > /dev/null 2>&1; then
-        test "$1" != -v || echo "$NAME is running."
-        return 0
-    else
-        test "$1" != -v || echo "$NAME is not running but $DHCP6SPID exists."
-        return 1
-    fi
+    for INT in $INTERFACES; do
+        if [ ! -r "${DHCP6SPIDBASE}.${INT}.pid" ]; then
+            test "$1" != -v || echo "$NAME is not running on ${INT}."
+            return 3
+        fi
+        if read pid < "${DHCP6SPIDBASE}.${INT}.pid" && ps -p "$pid" > /dev/null 2>&1; then
+            test "$1" != -v || echo "$NAME is running on ${INT}."
+            return 0
+        else
+            test "$1" != -v || echo "$NAME is not running on ${INT} but ${DHCP6SPIDBASE}.${INT}.pid exists."
+            return 1
+        fi
+    done
 }
 
 case "$1" in
 	start)
-		log_daemon_msg "Starting $DESC" "$NAME"
-		start-stop-daemon --start --quiet --pidfile $DHCP6SPID \
-			--exec $DHCP6SBIN -- $INTERFACES
-		sleep 2
-		if check_status -q; then
-			log_end_msg 0
-		else
-			log_end_msg 1
-			exit 1
-		fi
+                for INT in $INTERFACES; do
+                    log_daemon_msg "Starting $DESC on $INT" "$NAME"
+		    start-stop-daemon --start --quiet --pidfile ${DHCP6SPIDBASE}.${INT}.pid \
+			--exec $DHCP6SBIN -- -k /dev/null -P ${DHCP6SPIDBASE}.${INT}.pid $INT
+		    sleep 2
+		    if check_status -q; then
+                        log_end_msg 0
+                    else
+                        log_end_msg 1
+                        exit 1
+                    fi
+                done
 		;;
 	stop)
-		log_daemon_msg "Stopping $DESC" "$NAME"
-		start-stop-daemon --stop --quiet --pidfile $DHCP6SPID
-                log_end_msg $?
-		rm -f $DHCP6SPID
+                for INT in $INTERFACES; do
+                    log_daemon_msg "Stopping $DESC on $INT" "$NAME"
+                    start-stop-daemon --stop --quiet --pidfile ${DHCP6SPIDBASE}.${INT}.pid
+                    log_end_msg $?
+                    rm -f $DHCP6SPID
+                done
 		;;
 	restart|force-reload)
 		$0 stop
